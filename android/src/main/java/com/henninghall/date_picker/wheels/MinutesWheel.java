30ebe58547da94e1a04f2fea0cababf90e46bc35
zhendi <zhendi@seeapp.com>
2019-11-28 11:11:29 +0800

    hide ampm

--- android/src/main/java/com/henninghall/date_picker/wheels/MinutesWheel.java
+++ android/src/main/java/com/henninghall/date_picker/wheels/MinutesWheel.java
@@ -42,7 +42,7 @@ public class MinutesWheel extends Wheel {
 
     @Override
     public Paint.Align getTextAlign() {
-        return Settings.usesAmPm() ? Paint.Align.RIGHT: Paint.Align.LEFT;
+        return Paint.Align.LEFT;
     }
 
 }
